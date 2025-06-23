5330867e2cb441df087ee3efe25b4f0c842d733c
prasanna29 <prasannakumar@jazenetworks.com>
2025-06-23 18:46:25 +0530

    Initial Commit

---
+++ test/string_calculator_tdd_test.dart
@@ -0,0 +1,11 @@
+import 'package:string_calculator_tdd/string_calculator_tdd.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+  test('calculate', () {
+    expect(calculate(), 41);
+  });
+}
